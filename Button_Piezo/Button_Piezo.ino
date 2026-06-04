3c35a76508759d644c9cf4fb2b9eda707aba8bce
manno <manno@manno.nl>
2026-06-04 12:37:57 +0200

    Cleaned up code
    Removed unused EMA elements

--- Button_Piezo/Button_Piezo.ino
+++ Button_Piezo/Button_Piezo.ino
@@ -1,14 +1,9 @@
 // Using Joy-IT KY-012 (piezo) and KY-004 (button)
 
 int button = 10; // Declaration of the sensor input pin
-int value; // Temporary variable
+int value;       // Temporary variable
 
 const int piezoPin = 9;
-
-
-// Smoothing parameters (distance sensors _can_ be erratic, Exponential Moving Average (EMA) smoothes that out a bit)
-float alpha = .6;           // EMA smoothing factor (0.0 < alpha <= 1.0) 1.0 is no smoothing, 0.0 is max smoothing (flatline)
-float smoothedValue = 0.0;  // Initial smoothed value
 
 float duration, distance, smoothedDistance;
 
